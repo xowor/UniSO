eb6aca6d5490840371098c6c321561eebbc27b2f
Federica <federica.granata28@gmail.com>
2014-11-07 11:18:23 +0100

    Creazione progetto

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: federica
+ *
+ * Created on 7 novembre 2014, 10.55
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
