b5287a570731c3221b8564d1f279b6cd8ad3aab3
Luca <lbarbetti@live.it>
2014-12-10 15:24:12 +0100

    Semaphore P V

---
+++ src/semaphore.h
@@ -0,0 +1,10 @@
+#ifndef SEMAPHORE_H
+#define SEMAPHORE_H
+
+#include "semaphore.c"
+
+struct sembuf;
+int sem_p (int semid, int semnum);
+int sem_v (int semid, int semnum);
+
+#endif // SEMAPHORE_H
